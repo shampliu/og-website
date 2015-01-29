fd15ffc87ed7f357ed919329590fab1d757bec3b
Chang Liu <shampliu@gmail.com>
2015-01-29 03:35:08 -0800

    Bootstrapped

--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
-$('#ticker').easyTicker({
-	direction: 'up',
-	easing: 'swing',
-	speed: 'slow',
-	interval: 1600,
-	height: 'auto',
-	visible: 1,
-	mousePause: 0,
-	controls: {
-		up: '',
-		down: '',
-		toggle: '',
-		playText: 'Play',
-		stopText: 'Stop'
-	}
-});
-
 
