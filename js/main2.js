3c777c3911683056277ae00a2bdb8d8cb634e98a
Chang Liu <shampliu@gmail.com>
2015-02-19 12:32:17 -0800

    added 2 iterations, Themed & Skeleton

---
+++ js/main2.js
@@ -0,0 +1,122 @@
+///////////////////////////////		
+// Set Variables
+///////////////////////////////
+
+var gridContainer = $('.thumbs');
+var colW;
+var gridGutter = 0;
+var thumbWidth = 308;
+
+///////////////////////////////
+// Project Filtering 
+///////////////////////////////
+
+function filterInit() {
+	$('#filters a').click(function(){
+		var selector = $(this).attr('data-filter');	
+		console.log(selector);
+		$('.thumbs').isotope({
+			filter: selector,			
+			hiddenStyle : {
+		    	opacity: 0,
+		    	scale : 1
+			}			
+		});
+	
+		// if ( !$(this).hasClass('selected') ) {
+		// 	$(this).parents('#filterNav').find('.selected').removeClass('selected');
+		// 	$(this).addClass('selected');
+		// }
+	
+		return false;
+	});	
+}
+
+
+///////////////////////////////
+// Project thumbs 
+///////////////////////////////
+
+function isotopeInit() {
+	
+	// if(!isMobile()) {
+	// 	$(".project.small a").hover(
+	// 		function() {
+	// 			$(this).find('.overlay').stop().fadeTo("fast", .9);
+	// 			$(this).find('.description').stop().fadeTo("fast", 1);
+	// 			$(this).find('img:last').attr('title','');	
+	// 		},
+	// 		function() {
+	// 			$(this).find('.overlay').stop().fadeTo("fast", 0);	
+	// 			$(this).find('.description').stop().fadeTo("fast", 0);	
+	// 		});	
+	// }
+	
+	setColumns();	
+	gridContainer.isotope({		
+		resizable: false,
+		layoutMode: 'fitRows',
+		masonry: {
+			columnWidth: colW
+		}
+	});	
+	
+	$(".item").css("visibility", "visible");	
+	// $("#floatingCirclesG").fadeOut("slow");
+}
+
+///////////////////////////////////
+// Relocate Elements
+///////////////////////////////////
+
+// function relocateElements()
+// {	
+// 	if(jQuery('#container').width() <= 768) {
+// 		jQuery('#sidebar').insertAfter(jQuery('#content'));	
+// 		widgetsHidden = true;
+// 	}
+// 	else if(widgetsHidden) {
+// 		jQuery('#sidebar').insertAfter(jQuery('#mainNav'));
+// 	} 	
+// }
+
+///////////////////////////////
+// Isotope Grid Resize
+///////////////////////////////
+
+function setColumns()
+{	
+	var columns;
+	columns = Math.ceil(gridContainer.width()/thumbWidth);	
+	colW = Math.floor(gridContainer.width() / columns);
+	$('.thumbs .item').each(function(id){
+		$(this).css('width',colW-gridGutter+'px');
+	});
+}
+
+function gridResize() {	
+	setColumns();
+	gridContainer.isotope({
+		resizable: false,
+		masonry: {
+			columnWidth: colW
+		}
+	});		
+}
+
+///////////////////////////////
+// Initialize Page
+///////////////////////////////	
+
+$(window).load(function(){		
+	isotopeInit();	
+	filterInit();
+
+	// relocateElements();
+	
+	$(window).smartresize(function(){
+		gridResize();
+		sidebarAbsolute();
+		// relocateElements();
+	});		
+});
